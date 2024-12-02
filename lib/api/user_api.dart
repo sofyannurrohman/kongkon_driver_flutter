8c55ebd927e0781eca032bb7a31e151374db15e2
sofyannurrohman <sofyannurrohman1@gmail.com>
2024-12-02 15:41:28 +0700

    Create Sign Up

---
+++ lib/api/user_api.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class User {
+  
+  Future<Map<String, dynamic>> registerUser(data) async {
+    try {
+      final response = await http
+          .post(Uri.parse('http://localhost:3333/api/v1/users'));
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+          if (responseData['status'] == 'success' &&
+            responseData['data'] != null) {
+          final order = responseData['data']['order'];
+
+          final customerId = order['customer_id'];
+          final merchantId = order['merchant_id'];
+          if (customerId == null || merchantId == null) {
+            throw Exception('Missing user or merchant ID in the order data.');
+          }
+
+          return order;
+        } else {
+          throw Exception('Invalid response format');
+        }
+      } else {
+        throw Exception(
+            'Failed to load order details, status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching order details: $e');
+      throw Exception('Error fetching order details');
+    }
+  }
+
+
+}
--- lib/screen/sign_up_screen.dart
+++ lib/screen/sign_up_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:kongkon_app_driver/shared/theme.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _licenseController = TextEditingController();
+class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     Widget header() {
@@ -11,34 +19,170 @@ class SignUpScreen extends StatelessWidget {
         margin: const EdgeInsets.only(top: 30),
         width: 80,
         height: 80,
-        decoration: const BoxDecoration(image: DecorationImage(image: AssetImage('assets/dark-logo.png'))),
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/dark-logo.png'))),
       );
     }
 
     Widget title() {
       return Container(
-        margin: const EdgeInsets.only(top: 80, left: 20),
-        child: Text('Join Us as\nour Partner ',
-        style: blackTextStyle.copyWith(
-          fontSize: 24,
-          fontWeight: semibold
-        ),),
+        margin: const EdgeInsets.only(top: 40, left: 50),
+        child: Text(
+          'Join Us as\nour Partner ',
+          style: blackTextStyle.copyWith(fontSize: 24, fontWeight: semibold),
+        ),
       );
     }
 
-    Widget inputSection(){
-      return Container(
-        margin: const EdgeInsets.only(top: 30),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 30
-        ),
-        decoration: BoxDecoration(
-          color: kWhiteColor,
-          border: BorderRadius.circular(defaultRadius);
-        ),
-        child: Column(
+    Widget inputSection() {
+      Widget nameInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Full Name'),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                controller: _nameController,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: ' Your full name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius)),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
 
+      Widget emailInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Email Address'),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                controller: _emailController,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Your email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      borderSide: BorderSide(color: kGreyColor)),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget passwordInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Password'),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                  controller: _passwordController,
+                obscureText: true,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: ' Your password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius)),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget licenseInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('License Number'),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                  controller: _licenseController,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: ' Your license',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius)),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget submitButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+              onPressed: () async{
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: kPrimaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius)),
+              ),
+              child: Text(
+                'Register',
+                style:
+                    whiteTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+              )),
+        );
+      }
+
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+        decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(defaultRadius)),
+        child: Column(
+          children: [
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            licenseInput(),
+            submitButton()
+          ],
         ),
       );
     }
@@ -47,10 +191,7 @@ class SignUpScreen extends StatelessWidget {
       backgroundColor: kBackgroundColor,
       body: SafeArea(
         child: ListView(
-          children: [
-            header(),
-            title(),
-          ],
+          children: [header(), title(), inputSection()],
         ),
       ),
     );
